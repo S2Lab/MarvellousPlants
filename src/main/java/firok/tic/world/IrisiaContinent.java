026cb57f7095b28cfa38fb4454952df5287b56c8
Wither <351768593@outlook.com>
2017-12-23 01:58:57 +0800

    upload files

---
+++ src/main/java/firok/tic/world/IrisiaContinent.java
@@ -0,0 +1,10 @@
+package firok.tic.world;
+
+public class IrisiaContinent {
+	
+	public IrisiaContinent()
+	{
+		;
+	}
+
+}
---
+++ src/main/java/firok/tic/world/WorldLoader.java
@@ -0,0 +1,19 @@
+package firok.tic.world;
+
+import net.minecraft.world.DimensionType;
+
+public class WorldLoader {
+	
+	public static IrisiaContinent irisia;
+	
+	public WorldLoader()
+	{
+		irisia=new IrisiaContinent();
+	}
+	
+	protected void register(int idIn,DimensionType typeIn)
+	{
+		;
+	}
+
+}
